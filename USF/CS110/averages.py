85436f5e25f14ed500cb2cd9f9fcce42fd2e9331
AliceZhang97 <alicezhangyunyang@hotmail.com>
2017-08-02 23:15:44 +0800

    Create averages.py

---
+++ USF/CS110/averages.py
@@ -0,0 +1,14 @@
+__author__ = 'alicezhang'
+print('Please enter the first variable.')
+variable1 = input()
+
+print('Please enter the second variable.')
+variable2 = input()
+
+print('Please enter the third variable.')
+variable3 = input()
+
+number = int(variable1) + int(variable2) + int(variable3)
+average = int(number)/3
+print('The average of your three variables is ' + str(average) + '.')
+
